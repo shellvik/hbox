b4e114e8a8bcedcba784a61f904a50b773ff4ac5
shellvik <souvikbiswas6663@gmail.com>
2024-11-18 05:45:30 +0530

    fix

--- install.sh
+++ install.sh
@@ -1,48 +1,56 @@
-!#/bin/bash
+#!/bin/bash
 
-$(pwd)/src/vpn-scripts/banner.sh
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 rice() {
-  chmod +x $(pwd)/src/vpn-scripts/*.sh
-  sudo cp -R $(pwd)/src/vpn-scripts/* /opt
-  sudo cp -R $(pwd)/src/htb-icons/ /usr/share/icons/
-  [ -f "~/.bashrc" ] && sudo mv ~/.bashrc ~/.bashrc_bak
-  sudo cp $(pwd)/src/bashrc ~/.bashrc
+  chmod +x "$SCRIPT_DIR/src/vpn-scripts/"*.sh
+  sudo cp -R "$SCRIPT_DIR/src/vpn-scripts/"* /opt
+  sudo cp -R "$SCRIPT_DIR/src/htb-icons/" /usr/share/icons/
 
-  unzip $(pwd)/src/Material-Black-Lime-Numix-FLAT.zip
-  sudo cp -R $(pwd)/src/Material-Black-Lime-Numix-FLAT /usr/share/icons/
-  sudo chmod 777 /usr/share/icons/Material-Black-Lime-Numix-FLAT
+  [ -f "$HOME/.bashrc" ] && mv "$HOME/.bashrc" "$HOME/.bashrc_bak"
+  cp "$SCRIPT_DIR/src/bashrc" "$HOME/.bashrc"
 
-  sudo unzip $(pwd)/src/themes.zip
-  sudo mv /usr/share/themes/ themes_bak
-  sudo cp -R $(pwd)/src/themes /usr/share/themes
-  sudo chmod 777 /usr/share/themes
+  if [ -f "$SCRIPT_DIR/src/Material-Black-Lime-Numix-FLAT.zip" ]; then
+    unzip -d "$SCRIPT_DIR/src/Material-Black-Lime-Numix-FLAT" "$SCRIPT_DIR/src/Material-Black-Lime-Numix-FLAT.zip"
+    sudo cp -R "$SCRIPT_DIR/src/Material-Black-Lime-Numix-FLAT" /usr/share/icons/
+    sudo chmod 777 /usr/share/icons/Material-Black-Lime-Numix-FLAT
+  fi
 
-  sudo cp $(pwd)/src/wallpaper/* /usr/share/backgrounds/htb.jpg
+  if [ -f "$SCRIPT_DIR/src/themes.zip" ]; then
+    unzip -d "$SCRIPT_DIR/src/themes" "$SCRIPT_DIR/src/themes.zip"
+    sudo mv /usr/share/themes /usr/share/themes_bak
+    sudo cp -R "$SCRIPT_DIR/src/themes" /usr/share/themes
+    sudo chmod 777 /usr/share/themes
+  fi
 
-  #Fonts
-  [-d "~/.local/share/fonts/"] && sudo mkdir ~/.local/share/fonts
-  sudo cp -r $(pwd)/src/fonts/*.ttf ~/.local/share/fonts/
+  if [ -d "$SCRIPT_DIR/src/wallpaper" ]; then
+    sudo cp "$SCRIPT_DIR/src/wallpaper/"* /usr/share/backgrounds/htb.jpg
+  fi
 
+  if [ -d "$SCRIPT_DIR/src/fonts" ]; then
+    [ ! -d "$HOME/.local/share/fonts/" ] && mkdir -p "$HOME/.local/share/fonts"
+    cp -r "$SCRIPT_DIR/src/fonts/"*.ttf "$HOME/.local/share/fonts/"
+  fi
 }
 
 ansiinstall() {
-  sudo apt install pipx
+  sudo apt install -y pipx
+  pipx ensurepath
   pipx install ansible
   export LC_CTYPE="en_US.UTF-8"
   export LANG="en_US.UTF-8"
 }
 
 read -p "Run setup script? [Y/n] " setup
-if [[ $setup =~ ^[Yy] ]]; then
+if [[ $setup =~ ^[Yy]$ ]]; then
   rice
 else
-  echo "Cancelled!!..."
+  echo "Rice setup cancelled."
 fi
 
-read -p "Install ansible? [Y/n] " ansi
-if [[ $ansi =~ [Yy] ]]; then
+read -p "Install Ansible? [Y/n] " ansi
+if [[ $ansi =~ ^[Yy]$ ]]; then
   ansiinstall
 else
-  "Cancelled!!..."
+  echo "Ansible installation cancelled."
 fi
