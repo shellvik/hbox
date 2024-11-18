460b5ce16fb6303b5a7f4185c09dea0c72c6a5fe
shellvik <souvikbiswas6663@gmail.com>
2024-11-18 05:34:33 +0530

    fix typo

--- install.sh
+++ install.sh
@@ -1,33 +1,34 @@
 !#/bin/bash
 
-./src/vpn-scripts/banner.sh
+$(pwd)/src/vpn-scripts/banner.sh
 
 rice() {
-  chmod +x ./src/vpn-scripts/*.sh
-  sudo cp -R ./src/vpn-scripts/* /opt
-  sudo cp -R ./src/htb-icons/ /usr/share/icons/
-  sudo mv ~/.bashrc ~/bashrc_bak
-  sudo cp ./src/bashrc ~/.bashrc
+  chmod +x $(pwd)/src/vpn-scripts/*.sh
+  sudo cp -R $(pwd)/src/vpn-scripts/* /opt
+  sudo cp -R $(pwd)/src/htb-icons/ /usr/share/icons/
+  [-f "~/.bashrc" ] && sudo mv ~/.bashrc ~/.bashrc_bak
+  sudo cp $(pwd)/src/bashrc ~/.bashrc
 
-  unzip ./src/Material-Black-Lime-Numix-FLAT.zip
-  sudo cp -R ./src/Material-Black-Lime-Numix-FLAT /usr/share/icons/
+  unzip $(pwd)/src/Material-Black-Lime-Numix-FLAT.zip
+  sudo cp -R $(pwd)/src/Material-Black-Lime-Numix-FLAT /usr/share/icons/
   sudo chmod 777 /usr/share/icons/Material-Black-Lime-Numix-FLAT
 
-  sudo unzip ./src/themes.zip
+  sudo unzip $(pwd)/src/themes.zip
   sudo mv /usr/share/themes/ themes_bak
-  sudo cp -R ./src/themes /usr/share/themes
+  sudo cp -R $(pwd)/src/themes /usr/share/themes
   sudo chmod 777 /usr/share/themes
 
-  sudo cp ./src/wallpaper/* /usr/share/backgrounds/htb.jpg
+  sudo cp $(pwd)/src/wallpaper/* /usr/share/backgrounds/htb.jpg
 
   #Fonts
   [-d "~/.local/share/fonts/"] && sudo mkdir ~/.local/share/fonts
-  sudo cp -r ./src/fonts/*.ttf ~/.local/share/fonts/
+  sudo cp -r $(pwd)/src/fonts/*.ttf ~/.local/share/fonts/
 
 }
 
 ansiinstall() {
-  pip3 install ansible
+  sudo apt install pipx
+  pipx install ansible
   export LC_CTYPE="en_US.UTF-8"
   export LANG="en_US.UTF-8"
 }
@@ -39,7 +40,7 @@ else
   echo "Cancelled!!..."
 fi
 
-read -p "Install ansible?" ansi
+read -p "Install ansible? [Y/n] " ansi
 if [[ $ansi =~ [Yy] ]]; then
   ansiinstall
 else
